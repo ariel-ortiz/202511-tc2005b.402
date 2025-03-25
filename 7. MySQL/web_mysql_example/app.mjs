a78fe135e47c10ab8000cbcdeffa95efe0518dbb
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-03-25 18:59:31 +0000

    Add code for second version of express+mysql

--- 7. MySQL/web_mysql_example/app.mjs
+++ 7. MySQL/web_mysql_example/app.mjs
@@ -8,6 +8,29 @@ const ipAddress = process.env.C9_HOSTNAME ?? 'localhost';
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.get('/db', async (req, res) => {
+  let connection;
+  try {
+    connection = await mysql.createConnection({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: 'web_database'
+    });
+    
+    const [rows] = await connection.execute('SELECT * FROM quotations');
+    res.render('db', { rows });
+    
+  } catch (err) {
+    res.status(500).send('Error al acceder la base datos');
+    
+  } finally {
+    if (connection) {
+      await connection.end();
+    }
+  }
+});
+
 // Página de recurso no encontrado (estatus 404)
 app.use((req, res) => {
   const url = req.originalUrl;
